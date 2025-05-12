11f07a73fbb8d55447b4362981ceb6c356a62c0e
meklin89 <meklin89@gmail.com>
2025-05-11 19:14:12 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# ff1-rando-mek
+Final Fantasy 1 Randomizer Tracker
+
+Variant of FF1R Tracker created by jat2980. 
+
+Added new flags to update to current FFR options. 
+
+Added NOverworld tracker with built in map, with shards variant.
--- scripts/init.lua
+++ scripts/init.lua
@@ -7,15 +7,25 @@ Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/hosted_items.json")
 Tracker:AddItems("items/flags.json")
 
-if Tracker.ActiveVariantUID == "shardHunt" or Tracker.ActiveVariantUID == "shardHuntNoMap" then
+if Tracker.ActiveVariantUID == "shardHunt" or Tracker.ActiveVariantUID == "shardHuntNoMap" or Tracker.ActiveVariantUID == "shardHuntNOverworld" then
   Tracker:AddItems("shardHunt/shards.json")
 end
 
-Tracker:AddMaps("maps/maps.json")
+if Tracker.ActiveVariantUID == "NOverworld" or Tracker.ActiveVariantUID == "shardHuntNOverworld" then
+  Tracker:AddMaps("maps/NOverworldMaps.json")
+else
+  Tracker:AddMaps("maps/maps.json")
+end
 
 ScriptHost:LoadScript("scripts/logic.lua")
-Tracker:AddLocations("locations/locations.json")
-Tracker:AddLocations("locations/incentives.json")
+
+if Tracker.ActiveVariantUID == "NOverworld" or Tracker.ActiveVariantUID == "shardHuntNOverworld" then
+    Tracker:AddLocations("locations/NOverworld/locations.json")
+    Tracker:AddLocations("locations/NOverworld/incentives.json")
+else
+    Tracker:AddLocations("locations/locations.json")
+    Tracker:AddLocations("locations/incentives.json")
+end
 
 Tracker:AddLayouts("layouts/shared.json")
 if Tracker.ActiveVariantUID == "shardHunt" then
@@ -25,22 +35,28 @@ if Tracker.ActiveVariantUID == "shardHunt" then
   shardsRequired.CurrentStage = 8
 elseif Tracker.ActiveVariantUID == "shardHuntNoMap" then
   Tracker:AddLayouts("shardHuntNoMap/tracker.json")
+  Tracker:AddLayouts("shardHuntNoMap/broadcastNoMap.json")
+  local shardsRequired = Tracker:FindObjectForCode("shardsRequired")
+  shardsRequired.CurrentStage = 8
+elseif Tracker.ActiveVariantUID == "shardHuntNOverworld" then
+  Tracker:AddLayouts("NOverworld/shardsTracker.json")
   Tracker:AddLayouts("shardHunt/broadcast.json")
   local shardsRequired = Tracker:FindObjectForCode("shardsRequired")
   shardsRequired.CurrentStage = 8
+elseif Tracker.ActiveVariantUID == "NOverworld" then
+  Tracker:AddLayouts("NOverworld/tracker.json")
+  Tracker:AddLayouts("layouts/standard_broadcast.json")
 elseif Tracker.ActiveVariantUID == "standardNoMap" then
   Tracker:AddLayouts("standardNoMap/tracker.json")
-  Tracker:AddLayouts("layouts/standard_broadcast.json")
+  Tracker:AddLayouts("standardNoMap/broadcastNoMap.json")
 else
   Tracker:AddLayouts("layouts/tracker.json")
   Tracker:AddLayouts("layouts/standard_broadcast.json")
 end
 
 -- Default Flags
-local northernDocksFlag = Tracker:FindObjectForCode("northernDocks")
-northernDocksFlag.Active = true
 local progressionFlag = Tracker:FindObjectForCode("progressionFlag")
-progressionFlag.CurrentStage = 2
+progressionFlag.CurrentStage = 1
 local npcsIncentive = Tracker:FindObjectForCode("npcsAreIncentive")
 npcsIncentive.Active = true
 local fetchQuestsIncentive = Tracker:FindObjectForCode("fetchQuestsAreIncentive")
@@ -59,6 +75,7 @@ local skyIncentive = Tracker:FindObjectForCode("skyIsIncentive")
 skyIncentive.Active = true
 local coneriaLockedIncentive = Tracker:FindObjectForCode("coneriaLockedIsIncentive")
 coneriaLockedIncentive.Active = true
+
 
 Tracker.DisplayAllLocations = PREFERENCE_DISPLAY_ALL_LOCATIONS
 
