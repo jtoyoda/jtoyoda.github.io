dac7445e27636909eeb2b66a8590e654591c24eb
meklin89 <meklin89@gmail.com>
2025-08-10 19:50:51 -0500

    Update autotracking.lua

--- scripts/autotracking.lua
+++ scripts/autotracking.lua
@@ -1,5 +1,5 @@
 -- Configuration --------------------------------------
-AUTOTRACKER_ENABLE_DEBUG_LOGGING = true
+AUTOTRACKER_ENABLE_DEBUG_LOGGING = false
 -------------------------------------------------------
 
 print("")
@@ -113,11 +113,14 @@ function updateFloater(segment)
     if item then
         local floater = ReadU8(segment, 0x602B)
         local airship = ReadU8(segment, 0x6004)
+        local airboat = ReadU8(segment, 0x6080)
         if AUTOTRACKER_ENABLE_DEBUG_LOGGING then
             print(item.Name, floater, airship, item.CurrentStage)
         end
 
-        if airship > 0 then
+        if airboat >0 then
+            item.CurrentStage = 2
+        elseif airship > 0 then
             item.CurrentStage = 2
         elseif floater > 0 then
             item.CurrentStage = 1
